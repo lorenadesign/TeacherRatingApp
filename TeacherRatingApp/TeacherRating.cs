59d390ab446705231316deda414ef151f28be9d8
lorenadesign <94776600+lorenadesign@users.noreply.github.com>
2021-12-02 20:10:24 -0800

    Add files via upload

---
+++ TeacherRatingApp/TeacherRating.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherRatingApp
+{
+    class TeacherRating
+    {
+        string teacherNo;
+        string firstName;
+        string lastName;
+
+        //ratings
+        int easinessRating;
+        int helpfulnessRating;
+        int clarityRating;
+        double ratingAverage;
+
+        //default constructor
+        public TeacherRating()
+        {
+        }
+
+        public TeacherRating(string tNumber, string fName, string lName) 
+        {
+            teacherNo = tNumber;
+            firstName = fName;
+            lastName = lName;
+        }
+
+        // get and set properties for easinessRating
+        public int EasinessRating
+        {
+            get
+            {
+                return easinessRating;
+            }
+
+            set
+            {
+                easinessRating = value;
+            }
+        }
+
+        // get and set properties for helpfulnessRating
+        public int HelpfulnessRating
+        {
+            get
+            {
+                return helpfulnessRating;
+            }
+
+            set
+            {
+                helpfulnessRating = value;
+            }
+        }
+
+        // get and set properties for clarityRating
+       public int ClarityRating 
+        {
+            get
+            {
+                return clarityRating;
+            }
+
+            set
+            {
+                clarityRating = value;
+            }
+        }
+
+        public double RatingAverage(int eRating, int hRating, int cRating)
+        {
+            eRating = easinessRating;
+            hRating = helpfulnessRating;
+            cRating = clarityRating;
+            return (easinessRating + helpfulnessRating + clarityRating) / 3.0;
+            
+        }
+
+        public override string ToString()
+        {
+            return "\nTeacher Number: " + teacherNo 
+                + "\nTeacher Name: " + firstName + " " + lastName 
+                + "\nEasiness: " + easinessRating
+                + "\nHelpfulness: " + helpfulnessRating
+                + "\nClarity: " + clarityRating
+                + "\nAverage Rating: " + RatingAverage(EasinessRating, HelpfulnessRating, ClarityRating);
+        }
+    }
+}
---
+++ TeacherRatingApp/TeacherRatingApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TeacherRatingApp
+{
+    class TeacherRatingApp
+    {
+        static void Main(string[] args)
+        {
+            
+            DisplayInstructions();
+
+            string teacherID = GetInfo("Teacher Number");
+            string firstName = GetInfo("Teacher\'s First Name");
+            string lastName = GetInfo("Teacher\'s Last Name");
+            Console.WriteLine();
+            int score1 = int.Parse(GetInfo("Easiness Rating"));
+            int score2 = int.Parse(GetInfo("Helpfulness Rating"));
+            int score3 = int.Parse(GetInfo("Clarity Rating"));
+            TeacherRating firstStudent = new TeacherRating(teacherID,
+                          firstName, lastName);
+
+            firstStudent.EasinessRating = score1;
+            firstStudent.HelpfulnessRating = score2;
+            firstStudent.ClarityRating = score3;
+            
+            DisplayResults(firstStudent);
+        }
+
+        static void DisplayInstructions()
+        {
+            Console.WriteLine("You will be asked to enter a teacher ID number and three ratings.");
+            Console.WriteLine("The ratings relate to easiness, helpfulness, and clarity - in that order.");
+            Console.WriteLine("The information, including average rating will be displayed.");
+            Console.WriteLine("\n Ratings are based on a 1-5 scale with 1 being the lowest and 5 being the highest");
+            Console.WriteLine("\n\n Press any key when you are ready to begin...");
+            Console.ReadKey();
+        }
+
+        static string GetInfo(string info) 
+        {
+            string inputValue;
+
+            Console.Write("Please enter the {0}: ", info);
+            inputValue = Console.ReadLine();
+            return inputValue;
+        }
+
+        static void DisplayResults(TeacherRating tr)
+        {
+            Console.WriteLine(tr);
+        }
+        
+
+    }
+}
